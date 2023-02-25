5733e4d620b85371a78da34939fdc3e6d89af7e8
hp <hp@DESKTOP-B447EHA>
2023-02-25 16:21:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+public class PROGRAM
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please enter any string");
+        var Str = Console.ReadLine();  
+
+        int VowelsCounter = 0;
+        foreach (var item in Str)
+        {
+
+            if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' ||
+                item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+            {
+                VowelsCounter++;
+            }
+
+        }
+
+        Console.WriteLine("Number of vowels in the string = " + VowelsCounter);
+    }
+}
